633755d3ca3a9f91765bd8c91c7672727faa2af1
Mark Kogan <mkogan@redhat.com>
2020-05-29 12:36:51 +0300

    build script improvments

--- build_install.sh
+++ build_install.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+set -x
 
 # kernel version number from currently running kernel!
 
 KVER=`uname -r |cut -d- -f1`
-wget https://www.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
-tar xJvf linux-$KVER.tar.xz
+#wget https://www.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
+axel -a https://www.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
+nice ionice tar xJvf linux-$KVER.tar.xz
 cd linux-$KVER
 make mrproper
 cp /usr/lib/modules/$(uname -r)/build/.config ./
 cp /usr/lib/modules/$(uname -r)/build/Module.symvers ./
-make oldconfig
-make EXTRAVERSION=`uname -r|sed "s/$KVER//"` modules_prepare
+nice ionice make oldconfig -j$(nproc)
+nice ionice make EXTRAVERSION=`uname -r|sed "s/$KVER//"` modules_prepare -j$(nproc)
 patch -p1 < ../thinkpad_acpi.2ndfan.patch/thinkpad_acpi.2ndfan.patch
-make M=drivers/platform/x86
+nice ionice make M=drivers/platform/x86 -j$(nproc)
+cp -v drivers/platform/x86/thinkpad_acpi.ko ../
+modinfo ./thinkpad_acpi.ko
+make clean
+cd ..
+rm -rf linux-$KVER linux-$KVER.tar.xz
+exit 0
+
 xz -f drivers/platform/x86/thinkpad_acpi.ko
 mkdir /usr/lib/modules/`uname -r`/updates
 cp -f drivers/platform/x86/thinkpad_acpi.ko.xz /usr/lib/modules/`uname -r`/updates
