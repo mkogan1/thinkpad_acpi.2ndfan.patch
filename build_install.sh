c674f030c8b0634b61b6257e6c282876bfcf7809
Mark Kogan <mkogan@redhat.com>
2020-06-01 11:17:14 +0300

    tune build flow

--- build_install.sh
+++ build_install.sh
@@ -14,13 +14,14 @@ cp /usr/lib/modules/$(uname -r)/build/Module.symvers ./
 nice ionice make oldconfig -j$(nproc)
 nice ionice make EXTRAVERSION=`uname -r|sed "s/$KVER//"` modules_prepare -j$(nproc)
 patch -p1 < ../thinkpad_acpi.2ndfan.patch/thinkpad_acpi.2ndfan.patch
+#exit 1
 nice ionice make M=drivers/platform/x86 -j$(nproc)
 rm ../thinkpad_acpi.ko
 cp -v drivers/platform/x86/thinkpad_acpi.ko ../
 modinfo ../thinkpad_acpi.ko
-make clean
+#make clean
 cd ..
-rm -rf linux-$KVER linux-$KVER.tar.xz
+#rm -rf linux-$KVER linux-$KVER.tar.xz
 exit 0
 
 xz -f drivers/platform/x86/thinkpad_acpi.ko
