9bd177fafa9214bd1160a1a6eb0fd5c4ff1cf247
Mark Kogan <mkogan@redhat.com>
2020-07-20 13:46:51 +0300

    build clarification

--- build_install.sh
+++ build_install.sh
@@ -5,20 +5,25 @@ set -x
 
 KVER=`uname -r |cut -d- -f1`
 #wget https://www.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
-axel -a https://www.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
-nice ionice tar xJvf linux-$KVER.tar.xz
+rm -rf ./linux-$KVER
+#rm linux-$KVER.tar.xz
+if [[ ! -e linux-$KVER.tar.xz ]]; then
+  axel -a https://www.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
+fi
+#nice ionice tar xJvf linux-$KVER.tar.xz
+nice ionice tar xJf linux-$KVER.tar.xz
 cd linux-$KVER
-make mrproper
+nice make mrproper |& ccze -Aonolookups
 cp /usr/lib/modules/$(uname -r)/build/.config ./
 cp /usr/lib/modules/$(uname -r)/build/Module.symvers ./
-nice ionice make oldconfig -j$(nproc)
-nice ionice make EXTRAVERSION=`uname -r|sed "s/$KVER//"` modules_prepare -j$(nproc)
+nice ionice make oldconfig -j$(nproc) |& ccze -Aonolookups
+nice ionice make EXTRAVERSION=`uname -r|sed "s/$KVER//"` modules_prepare -j$(nproc) |& ccze -Aonolookups
 patch -p1 < ../thinkpad_acpi.2ndfan.patch/thinkpad_acpi.2ndfan.patch
 #exit 1
-nice ionice make M=drivers/platform/x86 -j$(nproc)
+nice ionice make M=drivers/platform/x86 -j$(nproc) |& ccze -Aonolookups
 rm ../thinkpad_acpi.ko
 cp -v drivers/platform/x86/thinkpad_acpi.ko ../
-modinfo ../thinkpad_acpi.ko
+modinfo ../thinkpad_acpi.ko |& ccze -Aonolookups
 #make clean
 cd ..
 #rm -rf linux-$KVER linux-$KVER.tar.xz
